0fd47d9cfe70a56369a324721a8ff14bdfab1b5f
Janvi <janvi@summachar.in>
2022-02-03 02:40:00 +0530

    error page ui improvements

--- pages/error.js
+++ pages/error.js
@@ -1,41 +1,55 @@
-import Link from 'next/Link'
-export default function Dashboard() {
+import Link from 'next/Link';
+import Head from 'next/head';
+
+export default function Error() {
     return (
-        <div>
-            <div className="screen">
 
-               
+        <div className="container">
+            <Head>
+                <title>Open Data Bayern</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <div className="screen">
 
-            </div>
-            <div >
+                    <form className="data-form">
 
-                <div className="card-grid">
-                    <div>
-                        <h1>🚫</h1>
-                        <h1>Entschuldigung, es liegt ein technischer Fehler vor.  Bitte wiederholen Sie die Aktion</h1>
-                    </div>
+                        <div className="row center">
+                            <div>
+                                <h1>🚫</h1>
+                                <h1>Entschuldigung, es liegt ein technischer Fehler vor.  Bitte wiederholen Sie die Aktion</h1>
+                            </div>
 
-                </div>
+                        </div>
 
-                <div className="bottom-btn">
-                    <Link href='/login'>
-                        <a>
-                            <button>
-                                Weiter zum Dashboard
+                        <div className="row center ">
+                            <Link href='/dashboard'>
+                                <a>
+                                    <button className="white-btn">
+                                        Weiter zum Dashboard
                             </button>
-                        </a>
-                    </Link>
+                                </a>
+                            </Link>
+
+                            <Link href='/upload'>
+                                <a>
+                                    <button className="black-btn">
+                                        Weitere Daten hochladen
+                                    </button>
+                                </a>
+                            </Link>
+                        </div>
+
+                    </form>
+
+
+
                 </div>
 
-                <div className="bottom-btn">
-                    <Link href='/login'>
-                        <a>
-                            <button>
-                                Wiederholen
-                            </button>
-                        </a>
-                    </Link>
-                </div>
-            </div>
-        </div>)
+            </main>
+
+
+        </div>
+
+    )
 }
