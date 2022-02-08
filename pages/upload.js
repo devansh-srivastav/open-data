69d19a18883e761884c908faf34e7ffd3ead30e4
Devansh Srivastav <devansh.srivastav@gmail.com>
2022-02-08 16:25:41 +0530

    Update upload.js

--- pages/upload.js
+++ pages/upload.js
@@ -55,7 +55,7 @@ export default function Home() {
                         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15 1.5H3C2.17125 1.5 1.5075 2.17125 1.5075 3L1.5 16.5L4.5 13.5H15C15.8288 13.5 16.5 12.8288 16.5 12V3C16.5 2.17125 15.8288 1.5 15 1.5ZM9.75 10.5H8.25V9H9.75V10.5ZM9.75 7.5H8.25V4.5H9.75V7.5Z" fill="black" />
                         </svg>
-                        <p className="s-12"><a href="mailto: olga.popova@dpschool.io" className="s-12">kontaktieren</a> Sie uns gerne, wenn Sie Hilfe beim Hochladen benötigen.</p>
+                        <p className="s-12"><a href="mailto: olga.popova@dpschool.io" className="s-12">Kontaktieren</a> Sie uns gerne, wenn Sie Hilfe beim Hochladen benötigen.</p>
 
                     </div>
 
